3031ad0a244614bda0018dc8b611a71f7c0cdb1a
suzuki57 <6450.tk.0507@gmail.com>
2022-04-29 22:41:09 +0900

    fifth commit

--- src/component/hero.tsx
+++ src/component/hero.tsx
@@ -7,8 +7,8 @@ export const Hero: React.FC = () => (
         <p className='text-[64px] font-bold'>良心ある自己実現が</p>
         <p className='text-[64px] font-bold'>できる世界へ</p>
         <p className='pt-16 text-[32px] font-bold'>貢献が正しく評価される仕組みを創る</p>
-        <p className='pt-32 pl-7 font-bold text-orange'>
-          \現在<span className='text-[32px]'>100名</span>がダウンロード/
+        <p className='pt-32 pl-4 font-bold text-orange'>
+          ＼現在<span className='text-[32px]'>100名</span>がダウンロード／
         </p>
         <button className='h-[76px] w-[292px] rounded bg-gradient-to-r from-orange to-yellow text-lg text-white hover:opacity-70 '>
           資料ダウンロード
--- src/component/pagefooter.tsx
+++ src/component/pagefooter.tsx
@@ -1,9 +1,46 @@
 import Image from 'next/image';
 import React from 'react';
 
-export const PageFooter: React.FC = () => 
-  (
-    <div>
+export const PageFooter: React.FC = () => (
+  <div className='h-[700px] bg-gray-700 p-10 text-white'>
+    <div className='border-b-4 border-white'>
+      <div className='flex-3 px-10 py-5 text-left'>
+        <Image src='/img/logo.png' alt='logo' width={150} height={60}></Image>
+      </div>
+      <p className='text-lg font-bold'>良き仲間との出会い人生最大の財産である</p>
+      <p className='pt-5 text-lg'>＼SNSでシェア／</p>
 
-    </div> 
-  )
+      <div className='flex'>
+        <div className='mr-5 h-[62px] w-[62px] rounded bg-white'>
+          <div className='p-3'>
+            <Image src='/icon/facebook.png' alt='facebook' width={35} height={40}></Image>
+          </div>
+        </div>
+
+        <div className='mr-5 h-[62px] w-[62px] rounded bg-white'>
+          <div className='p-3'>
+            <Image src='/icon/twitter.png' alt='twitter' width={35} height={40}></Image>
+          </div>
+        </div>
+
+        <div className='mr-5 h-[62px] w-[62px] rounded bg-white'>
+          <div className='p-3'>
+            <Image src='/icon/line.png' alt='line' width={35} height={40}></Image>
+          </div>
+        </div>
+
+        <div className='mr-5 h-[62px] w-[62px] rounded bg-white'>
+          <div className='p-3'>
+            <Image src='/icon/hatebu.png' alt='hatebu' width={35} height={40}></Image>
+          </div>
+        </div>
+
+        <div className='h-[62px] w-[62px] rounded bg-white'>
+          <div className='p-3'>
+            <Image src='/icon/pinterest.png' alt='pinterest' width={35} height={40}></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
