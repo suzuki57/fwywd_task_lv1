e2e3563674c02d67057f0f328ceaa964e4007e2c
suzuki57 <6450.tk.0507@gmail.com>
2022-04-29 13:32:07 +0900

    second commit

--- src/component/hero.tsx
+++ src/component/hero.tsx
@@ -1,21 +1,19 @@
-import Image from 'next/image';
 import React from 'react';
 
-export const Hero: React.FC = () => 
-  (
-    <div>
-      <div className='bg-hero bg-cover h-[950px]'>
-        <div>
-          <p className="text-[64px] font-bold">良心ある自己実現</p>
-          <p className="text-[64px] font-bold">ができる世界へ</p>
-
-          <p className="text-[32px] font-bold">貢献が正しく評価される仕組みを創る</p>
-
-          <p className="text-orange font-bold">\現在<span className="text-[32px]">100名</span>がダウンロード/</p>
-
-          <button className="rounded h-[76px] w-[292px] text-white text-lg bg-gradient-to-r from-orange to-yellow hover:opacity-70 ">資料ダウンロード</button>
-        </div>
-
+export const Hero: React.FC = () => (
+  <div>
+    <div className='h-[960px] bg-hero bg-cover'>
+      <div className='pl-16 pt-52'>
+        <p className='text-[64px] font-bold'>良心ある自己実現が</p>
+        <p className='text-[64px] font-bold'>できる世界へ</p>
+        <p className='pt-16 text-[32px] font-bold'>貢献が正しく評価される仕組みを創る</p>
+        <p className='pt-32 pl-7 font-bold text-orange'>
+          \現在<span className='text-[32px]'>100名</span>がダウンロード/
+        </p>
+        <button className='h-[76px] w-[292px] rounded bg-gradient-to-r from-orange to-yellow text-lg text-white hover:opacity-70 '>
+          資料ダウンロード
+        </button>
       </div>
-    </div> 
-    );
+    </div>
+  </div>
+);
--- src/component/sympathy.tsx
+++ src/component/sympathy.tsx
@@ -1,14 +1,62 @@
 import Image from 'next/image';
 import React from 'react';
 
-export const Sympathy: React.FC = () => 
-  (
-    <div className="h-[1100px] bg-primary-50">
-      <p>夢を叶える仲間が欲しい、あなたへ</p>
-      <p>新しいことを始めたい気持ちは強い</p>
-      <p></p>
-      <p></p>
+export const Sympathy: React.FC = () => (
+  <div className='h-[1100px] bg-primary-50'>
+    <p className='m-auto mb-16 w-[36%] border-b-4 border-yellow pt-28 text-center text-2xl font-bold text-primary-800'>
+      夢を叶える仲間が欲しい、あなたへ
+    </p>
+    <div className='m-auto flex w-[36%] text-lg font-bold text-primary-800'>
+      <div className='flex-10'>
+        <Image src='/icon/check.png' alt='check' width={40} height={40}></Image>
+      </div>
+      <p className='flex-1 pt-1'>新しいことを始めたい気持ちは強い</p>
+    </div>
+    <div className='m-auto flex w-[36%] text-lg font-bold text-primary-800'>
+      <div className='flex-10'>
+        <Image src='/icon/check.png' alt='check' width={40} height={40}></Image>
+      </div>
+      <p className='flex-1 pt-1'>学びに必要な教材がたくさんあることは知っている</p>
+    </div>
+    <div className='m-auto flex w-[36%] text-lg font-bold text-primary-800'>
+      <div className='flex-10'>
+        <Image src='/icon/check.png' alt='check' width={40} height={40}></Image>
+      </div>
+      <p className='flex-1 pt-1'>最後のひと押しの勇気が欲しい</p>
+    </div>
 
+    <div className='m-auto flex w-9/12 pt-10'>
+      <div className='flex-1'>
+        <div className='m-auto h-[614px] w-[500px]  rounded bg-white text-center text-lg font-bold shadow-lg'>
+          <p className='w-1/6 bg-primary-800 text-[18px] text-white'>悩み１</p>
+          <div className='text-primary-800'>
+            <p className='m-auto w-[35%] border-b-4 border-yellow pt-8'>
+              <span className='text-2xl'>仲間</span>がいない
+            </p>
+            <div className='py-8'>
+              <Image src='/img/sympathy1.png' alt='sympathy1' width={315} height={315}></Image>
+            </div>
+            <p>会社の同僚や友人と一緒に</p>
+            <p>学ぶのは難しい</p>
+          </div>
+        </div>
+      </div>
 
-    </div> 
-    )
+      <div className='flex-1'>
+        <div className='m-auto h-[614px] w-[500px]  rounded bg-white text-center text-lg font-bold shadow-lg'>
+          <p className='w-1/6 bg-primary-800 text-[18px] text-white'>悩み２</p>
+          <div className='text-primary-800'>
+            <p className='m-auto w-[40%] border-b-4 border-yellow pt-8'>
+              <span className='text-2xl'>目的</span>があいまい
+            </p>
+            <div className='py-8'>
+              <Image src='/img/sympathy2.png' alt='sympathy2' width={315} height={315}></Image>
+            </div>
+            <p>ゆるい繋がりのコミュニティでは</p>
+            <p>目的が達成できない</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
